0939b1002a5577ee3ea8d287041663dcc66ae8e0
Jimmy Morrens <morrensj@gmail.com>
2020-05-24 17:50:32 +0200

    FEAT: Responsive

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,6 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 // Sections
-import SectionHeader from "../components/sections/header"
 import Presentation from "../components/sections/presentation"
 import Digit from "../components/sections/digit";
 import Contact from "../components/sections/contact"
@@ -16,7 +15,6 @@ import Timeline from "../components/sections/timeline";
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    {/*<SectionHeader />*/}
     <Presentation />
     <Timeline />
     <Services />
