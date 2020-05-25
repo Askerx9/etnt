044b397abe11e469018ee3bca8d424107a6be8c3
Jimmy Morrens <morrensj@gmail.com>
2020-05-25 20:31:39 +0200

    FIX: debug

--- src/components/modules/fadInSection.js
+++ src/components/modules/fadInSection.js
@@ -1,35 +1,28 @@
-
-import React, {useRef, useEffect} from "react";
+import React from "react";
 
 const FadeInSection = (props) => {
-    // const controller = new ScrollMagic.Controller();
-    // const domRef = useRef();
-    //
-    // useEffect(()=>{
-    //   new ScrollMagic.Scene({
-    //     triggerElement: domRef.current,
-    //   })
-    //       .setClassToggle(domRef.current, "isVisible") // pins the element for the the scene's duration
-    //       .addTo(controller); // assign the scene to the controller
-    //
-    //
-    // },[domRef])
 
-    const [isVisible, setVisible] = React.useState(false);
     const domRef = React.useRef();
+    let width = window.innerWidth;
+
+    window.addEventListener('resize', () => {width = window.innerWidth})
     React.useEffect(() => {
-        const observer = new IntersectionObserver(entries => {
-            entries.forEach(entry => {
-                entry.target.classList.add('fade-in-section')
-                if(entry.isIntersecting) {
-                    entry.target.classList.add('isVisible')
-                }
 
-            });
-        }, {threshold: 0.5});
+        if(IntersectionObserver !== undefined && width > 678) {
+            const observer = new IntersectionObserver(entries => {
+                entries.forEach(entry => {
+                    entry.target.classList.add('fade-in-section')
+                    if(entry.isIntersecting) {
+                        entry.target.classList.add('isVisible')
+                    }
 
-        observer.observe(domRef.current);
-        return () => observer.unobserve(domRef.current);
+                });
+            }, {threshold: 0.5});
+
+            observer.observe(domRef.current);
+            return () => observer.unobserve(domRef.current);
+        }
+
     }, []);
 
     return (
--- src/components/sections/presentation.js
+++ src/components/sections/presentation.js
@@ -1,37 +1,43 @@
-import React, {useEffect, useRef} from "react"
+import React from "react"
 import ParticlesBackground from "../modules/particlesBackground";
+
+import fingerPrint from "../../images/fingerprint.svg"
 
 const Presentation = () => {
 
   return (
     <section className={"section__presentation"} id={"presentation"}>
       <div className={"container"}>
-        <div className={"section_title"}>
-          <h2>NOTRE HISTOIRE</h2>
-          <p>qui sommes-nous ?</p>
+        <div>
+          <div className={"section_title"}>
+            <h2>NOTRE HISTOIRE</h2>
+            <p>qui sommes-nous ?</p>
+          </div>
+          <div className={"section__presentation_content"}>
+            <p>
+              Créé en 2012, la société ETNT est devenue aujourd’hui une référence dans le déploiement
+              de fibre optique FTTH en France.
+            </p>
+            <p>
+              Bureau d’étude, spécialisé en ingénierie télécom, reconnu depuis plusieurs années passées
+              au service de grands groupes français leader sur ce marché.
+            </p>
+            <p>
+              Aujourd’hui, à travers notre équipe soudée d’hommes et de femmes, notre société
+              compte à son actif la réalisation de plusieurs milliers d’études FTTH.
+              Notre empreinte s’étend des rues de Paris, à celles de Marseille, tout en passant par
+              Lyon, mais aussi Toulouse, Lille, Bordeaux, Rouen, Nancy, Annecy, Reims, Roubaix, Cholet,
+              Annemasse, Le Mans, Nantes, Brest, Aix en Provence et tant d’autres.
+            </p>
+            <p>
+              ETNT, est un acteur à part entière dans le «Plan France 100 % Très Haut Débit » ainsi que
+              dans le développement de smart cities
+            </p>
+          </div>
         </div>
-        <div className={"section__presentation_content"}>
-          <p>
-            Créé en 2012, la société ETNT est devenue aujourd’hui une référence dans le déploiement
-            de fibre optique FTTH en France.
-          </p>
-          <p>
-            Bureau d’étude, spécialisé en ingénierie télécom, reconnu depuis plusieurs années passées
-            au service de grands groupes français leader sur ce marché.
-          </p>
-          <p>
-            Aujourd’hui, à travers notre équipe soudée d’hommes et de femmes, notre société
-            compte à son actif la réalisation de plusieurs milliers d’études FTTH.
-            Notre empreinte s’étend des rues de Paris, à celles de Marseille, tout en passant par
-            Lyon, mais aussi Toulouse, Lille, Bordeaux, Rouen, Nancy, Annecy, Reims, Roubaix, Cholet,
-            Annemasse, Le Mans, Nantes, Brest, Aix en Provence et tant d’autres.
-          </p>
-          <p>
-            ETNT, est un acteur à part entière dans le «Plan France 100 % Très Haut Débit » ainsi que
-            dans le développement de smart cities
-          </p>
-        </div>
+        <img src={fingerPrint} alt=""/>
       </div>
+
       <ParticlesBackground/>
     </section>
   )
