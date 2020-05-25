8d34c0fca4a50ab8c1d51035f80a66ab07c0c8e3
Jimmy Morrens <morrensj@gmail.com>
2020-05-25 14:22:49 +0200

    FIX: mailto

--- src/components/sections/contact.js
+++ src/components/sections/contact.js
@@ -27,12 +27,12 @@ const Contact = () => {
                   PAVILLON INTERNATIONAL 504<br/>
                   9000 TANGER - MAROC<br/>
                 </p>
-                  <a href={"mailto:cem@etnt-group.com"}>cem@etnt-group.com</a>
               </li>
 
               <li className="info__el">
                 <p><span className="bold">Tél Maroc&nbsp;:</span> <a href="tel:+212652407050">+212 6 52 40 70 50</a></p>
                 <p><span className="bold">Tél France&nbsp;:</span> <a href="tel:+33613733410">+33 6 13 73 34 10</a></p>
+                <p><span className="bold">Mail&nbsp;:</span> <a href="mailto:cem@etnt-group.com">cem@etnt-group.com</a></p>
               </li>
             </ul>
           </div>
